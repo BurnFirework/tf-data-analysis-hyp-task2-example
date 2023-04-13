33cc9e33b40ef9cae390ff7702b6db6667621298
BurnFirework <99072007+BurnFirework@users.noreply.github.com>
2023-04-12 23:04:14 -0700

    improved solution

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ chat_id = 860138765
 def solution(x: np.array, y: np.array) -> bool:
     imp_crit = 0.03
     t, p_value = ttest_ind(x, y, equal_var=False)
-    return p_value < imp_crit
+    return (not (p_value < imp_crit))
